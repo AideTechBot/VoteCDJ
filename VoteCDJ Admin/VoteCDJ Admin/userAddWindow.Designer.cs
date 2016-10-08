4c5424a8db0ed769e0c645fc50ef0c8882f8fbb2
Manuel Dionne <mandio0302@gmail.com>
2016-10-08 17:17:18 -0400

    Revamped user modifiying UI and features
    You can now change usernames while the vote is going, search the
    usernames and change passwords.

--- VoteCDJ Admin/VoteCDJ Admin/userAddWindow.Designer.cs
+++ VoteCDJ Admin/VoteCDJ Admin/userAddWindow.Designer.cs
@@ -36,14 +36,21 @@
             this.importButton = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
+            this.search_Bar = new System.Windows.Forms.TextBox();
+            this.search_Button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox
             // 
+            this.listBox.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(12, 12);
+            this.listBox.ItemHeight = 17;
+            this.listBox.Location = new System.Drawing.Point(12, 55);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(177, 329);
+            this.listBox.Size = new System.Drawing.Size(177, 276);
             this.listBox.TabIndex = 0;
             this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
             // 
@@ -60,7 +67,7 @@
             // gradeLabel
             // 
             this.gradeLabel.AutoSize = true;
-            this.gradeLabel.Location = new System.Drawing.Point(195, 12);
+            this.gradeLabel.Location = new System.Drawing.Point(195, 210);
             this.gradeLabel.Name = "gradeLabel";
             this.gradeLabel.Size = new System.Drawing.Size(42, 13);
             this.gradeLabel.TabIndex = 3;
@@ -68,7 +75,7 @@
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(195, 127);
+            this.deleteButton.Location = new System.Drawing.Point(195, 113);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(131, 23);
             this.deleteButton.TabIndex = 4;
@@ -78,7 +85,7 @@
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(195, 98);
+            this.addButton.Location = new System.Drawing.Point(195, 84);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(131, 23);
             this.addButton.TabIndex = 5;
@@ -88,7 +95,7 @@
             // 
             // importButton
             // 
-            this.importButton.Location = new System.Drawing.Point(195, 69);
+            this.importButton.Location = new System.Drawing.Point(195, 55);
             this.importButton.Name = "importButton";
             this.importButton.Size = new System.Drawing.Size(131, 23);
             this.importButton.TabIndex = 6;
@@ -102,7 +109,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(195, 156);
+            this.button2.Location = new System.Drawing.Point(195, 142);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(131, 23);
             this.button2.TabIndex = 7;
@@ -110,11 +117,64 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.clear_Click);
             // 
+            // search_Bar
+            // 
+            this.search_Bar.Location = new System.Drawing.Point(12, 12);
+            this.search_Bar.Name = "search_Bar";
+            this.search_Bar.Size = new System.Drawing.Size(177, 20);
+            this.search_Bar.TabIndex = 8;
+            this.search_Bar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.search_Bar_KeyDown);
+            // 
+            // search_Button
+            // 
+            this.search_Button.Location = new System.Drawing.Point(195, 10);
+            this.search_Button.Name = "search_Button";
+            this.search_Button.Size = new System.Drawing.Size(92, 23);
+            this.search_Button.TabIndex = 9;
+            this.search_Button.Text = "Chercher";
+            this.search_Button.UseVisualStyleBackColor = true;
+            this.search_Button.Click += new System.EventHandler(this.search_Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 36);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Changer le mot de passe";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.changePass_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nom d\'utilisateur";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "A voté";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
             // userAddWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(338, 355);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.search_Button);
+            this.Controls.Add(this.search_Bar);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.importButton);
             this.Controls.Add(this.addButton);
@@ -146,5 +206,10 @@
         private System.Windows.Forms.Button importButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox search_Bar;
+        private System.Windows.Forms.Button search_Button;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
