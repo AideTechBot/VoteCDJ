354864dce8fe6a3a560e25f645503b0c7b8c1a7e
Manuel Dionne <mandio0302@gmail.com>
2016-10-08 00:31:59 -0400

    Added password changing

---
+++ VoteCDJ Admin/VoteCDJ Admin/changePasswd.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VoteCDJ_Admin
+{
+    partial class changePasswd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(259, 20);
+            this.textBox.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(196, 38);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Changer";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // changePasswd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 70);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Name = "changePasswd";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Changement de mot de passe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+    }
+}
