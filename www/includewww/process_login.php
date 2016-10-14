6bf1bc19e35fe9281cfb4ce91b9b0bc67eb31168
Manuel Dionne <mandio0302@gmail.com>
2016-10-14 08:42:17 -0400

    Disables passwords
    DO NOT USE, PLEASE DON'T

--- www/includewww/process_login.php
+++ www/includewww/process_login.php
@@ -3,7 +3,8 @@ include_once 'db_connect.php';
 include_once 'functions.php';
 sec_session_start(); // Our custom secure way of starting a PHP session.
 
-if (isset($_POST['username'], $_POST['p'])) {
+
+if (isset($_POST['username'])) {
     $username = $_POST['username'];
     if(!isset($_POST['p']))
     {
