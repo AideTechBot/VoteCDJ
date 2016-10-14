947d1bfebb43f762b0be3f539dba64cd075c8598
Manuel Dionne <mandio0302@gmail.com>
2016-10-14 08:41:36 -0400

    Added a way to send password in plaintext
    NEVER ACTUALLY USE THIS

--- www/includewww/process_login.php
+++ www/includewww/process_login.php
@@ -1,12 +1,18 @@
 <?php
 include_once 'db_connect.php';
 include_once 'functions.php';
- 
 sec_session_start(); // Our custom secure way of starting a PHP session.
 
 if (isset($_POST['username'], $_POST['p'])) {
     $username = $_POST['username'];
-    $password = $_POST['p']; // The hashed password.
+    if(!isset($_POST['p']))
+    {
+        $password = hash('sha512', $password);
+    }
+    else
+    {
+        $password = $_POST['p']; // The hashed password.
+    }
  
     if (login($username, $password, $mysqli) == 1) {
         // Login success 
@@ -22,4 +28,6 @@ if (isset($_POST['username'], $_POST['p'])) {
 } else {
     // The correct POST variables were not sent to this page. 
     echo 'Invalid Request';
+    error_log("Invalid Request");
 }
+?>
